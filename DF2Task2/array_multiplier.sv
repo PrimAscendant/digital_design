2c03034be383f4c8579a4307ae521ff30da70897
Tommybucksf <firstsamurais@gmail.com>
2025-02-04 02:05:38 +0200

    Improve and generalize array multiplier addition logic

--- DF2Task2/array_multiplier.sv
+++ DF2Task2/array_multiplier.sv
@@ -1,14 +1,3 @@
-//4x4 Array Multiplier
-module one_bit_and(
-  input  logic a_i,
-  input  logic b_i,
-  output logic c_o
-);
-
-  assign c_o = a_i & b_i;
-endmodule
-
-
 module array_multiplier #(parameter N = 4) (
   input  logic [N-1:0] a_i,
   input  logic [N-1:0] b_i,
@@ -17,14 +6,14 @@ module array_multiplier #(parameter N = 4) (
 
   logic [N-1:0] pp_row [N-1:0];
   logic [2*N-1:0] shifted_row [N-1:0];
-  logic [2*N-1:0] sum0, sum1;
+  logic [2*N-1:0] sum0 [0:N];
   
   genvar i, j;
 
   generate
     for(i = 0; i < N; i++) begin : gen_pp_row
       for(j = 0; j < N; j++) begin : gen_pp_bit
-        one_bit_and fa(a_i[j],b_i[i],pp_row[i][j]);
+        assign pp_row[i][j] = a_i[j] & b_i[i];
       end
     end
   endgenerate 
@@ -34,10 +23,16 @@ module array_multiplier #(parameter N = 4) (
       assign shifted_row[i] = {{(N - i){1'b0}}, pp_row[i], {i{1'b0}}};
     end
   endgenerate
-  // Two-level addition of shifted rows
-  assign sum0 = shifted_row[0] + shifted_row[1];
-  assign sum1 = shifted_row[2] + shifted_row[3];
-  assign product_o = sum0 + sum1;
-
+  
+  assign sum0[0] = {2*N{1'b0}};
+  
+   // Accumulate partial sums over N rows
+  generate
+    for(i = 0; i < N; i++) begin
+      assign sum0[i+1] = sum0[i] + shifted_row[i];
+    end
+  endgenerate
+  
+  
+assign product_o = sum0[N];
 endmodule
-
