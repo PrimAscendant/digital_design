4eccf350f8c070c19292e7198cde3222018eaf78
Tommybucksf <firstsamurais@gmail.com>
2025-02-25 01:42:19 +0200

    update sequence detector with new version

--- DF2Task3/sequence_detector_fsm/sequence_detector_fsm.sv
+++ DF2Task3/sequence_detector_fsm/sequence_detector_fsm.sv
@@ -1,34 +1,70 @@
-// 05.02
-// v 0.3
+// 
+// v 0.5
 // simple Sequence Detector with FSM
 
+
 module fsm(
-  input logic in_i,
-  input logic clk, 
+ input logic in_i,
+  input logic clk,
   input logic reset_i,
   output logic detected_o
 );
 
-  localparam [3:0] find_sequence = 4'b1011;
-  
-  logic [3:0] temp_sequence;
+  typedef enum logic [2:0] {
+    S0, // no bits matched
+    S1, // matched '1'
+    S2, // matched '10'
+    S3, // matched '101'
+    S4  // matched '1011'
+  } state_t;
+
+  state_t state, next;
+
+  always_comb begin
+    // Default to no change
+    next = state;
+    case (state)
+      S0: begin
+        // if in == 1 then go else, defoult state
+        if (in_i == 1'b1) next = S1;
+        else            next = S0;
+      end
+
+      S1: begin
+        if (in_i == 1'b1) next = S2;
+        else            next = S1;
+      end
+
+      S2: begin
+        if (in_i == 1'b1) next = S3;
+        else            next = S0;
+      end
+
+      S3: begin
+        if (in_i == 1'b1) next = S4;
+        else            next = S2;
+      end
+
+      S4: begin
+        if (in_i == 1'b1) next = S1;
+        else            next = S0;
+      end
+
+      default: next = S0;
+    endcase
+  end
 
 
-  always_ff @(posedge clk or posedge reset_i) begin
+  always_ff @(posedge clk or negedge reset_i) begin
     if (reset_i) begin
-      temp_sequence <= 4'b0000;
-      detected_o <= 1'b0;
-  end
-    else begin
-      temp_sequence <= {temp_sequence[2:0], in_i};
-      if (temp_sequence == find_sequence) begin
-        detected_o <= 1'b1;
-        temp_sequence <= 4'b0000;
-    end 
-    else begin
-        detected_o <= 1'b0;
+      state <= S0;
+      detected_o     <= 1'b0;
     end
+    else begin
+      state <= next;
+     detected_o <= (next == S4);
     end
   end
+
 endmodule
 
